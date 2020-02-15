8302453837cd01f17a26ec517cc216e04bcb8acf
Denis Olekh <denisolekh@gmail.com>
2020-02-16 00:01:13 +0200

    hw

--- 2.data_types/1/index.js
+++ 2.data_types/1/index.js
@@ -3,6 +3,6 @@ const greeting = "I'm John";
 const _profession = 'Developer';
 const $hobby = 'Walking';
 const favouriteDessert = 'cake';
-let pet_ = 'dog';
+let pet = 'dog';
 let superNumber;
 superNumber = 17;

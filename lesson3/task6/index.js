62bda114b13acfb239dee1be9d00355e7d8b38fc
Denis Olekh <denisolekh@gmail.com>
2020-02-16 16:43:14 +0200

    hw

--- lesson3/task6/index.js
+++ lesson3/task6/index.js
@@ -1,4 +1,3 @@
-/* Этот код менять не нужно */
 const defaultUserName = 'anonymus';
 const userName = 'John';
 const anotherUser = null;
@@ -7,8 +6,7 @@ const message = 'text';
 const anotherMessage = 'text as well';
 const isLoggedIn = Boolean(userName);
 
-/* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser + defaultUserName;
+const activeUser = defaultUserName;
 const hasAccess = isAdmin != isLoggedIn;
 isTruthy = message < anotherMessage;
 isFalsy = !isLoggedIn;

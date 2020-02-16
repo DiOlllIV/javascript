22341a7b3ccb908dfd136e06d0da77d212d894e9
Denis Olekh <denisolekh@gmail.com>
2020-02-16 15:56:00 +0200

    hw

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -6,8 +6,8 @@ console.log("a <= b: ", a <= b);
 console.log("a == b: ", a == b);
 console.log("a != b: ", a != b);
 
-console.log("'a' > 'b': ", 'a' > 'b');
 console.log("'a' < 'b': ", 'a' < 'b');
+console.log("'ab' > 'b': ", 'ab' > 'b');
 
 console.log("17 === 1: ", 17 === 1);
 console.log("17 === 17: ", 17 === 17);
@@ -16,7 +16,7 @@ console.log("17 === true: ", 17 === true);
 console.log("'0' === '': ", '0' === '');
 console.log("true === false: ", true === false);
 console.log("true === true: ", true === true);
-console.log("null === undefied: ", null === undefined);
+console.log("null === undefined: ", null === undefined);
 console.log("false === 0: ", false === 0);
 
 console.log("17 == '17': ", 17 == '17');
@@ -25,8 +25,8 @@ console.log("0 == '': ", 0 == '');
 console.log("null == undefined: ", null == undefined);
 console.log("false == 0: ", false == 0);
 
-console.log("undefued == null: ", undefined == null);
-console.log("undefued == 0: ", undefined == 0);
+console.log("undefined == null: ", undefined == null);
+console.log("undefined == 0: ", undefined == 0);
 console.log("null == 0: ", null == 0);
-console.log("undefued < 0: ", undefined < 0);
-console.log("undefued > 0: ", undefined > 0);
+console.log("undefined < 0: ", undefined < 0);
+console.log("undefined > 0: ", undefined > 0);

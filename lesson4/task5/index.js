d7815c8d24ad46fe09b1b713782f5192ce83fec3
Denis Olekh <denisolekh@gmail.com>
2020-02-17 14:39:31 +0200

    hw

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,17 +1,22 @@
 const m = 2;
-const n = 5;
+const n = 6;
 let i = m;
 let result = 0;
 
 for (i = 0; i <= n; i++) {
 
-    if (i % 2 == 0) {
+    if (i % 2 == 0 && i % 4 == 1) {
         result += 2;
+        continue;
 
     } else if (i / 3 == 0) {
         result -= 3;
+        continue;
 
-    } else(i / 4 == 0)
+    } else if (i / 4 == 0) {
+        result *= 4;
+        continue;
+    }
 
     if (i % 5 == 0) {
         console.log(i);

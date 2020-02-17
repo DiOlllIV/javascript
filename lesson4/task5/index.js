96404d3189d92bc9145ddabd8c9db6a346fe7f18
Denis Olekh <denisolekh@gmail.com>
2020-02-17 12:33:29 +0200

    hw

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,20 +1,26 @@
 const m = 2;
 const n = 20;
 let i = m;
+let sum = 0
 let result = 0;
 
 for (i = 0; i <= n; i++) {
-    if (i == 2 && i !== 4) {
-        result = i + 2;
-        console.log(result);
-    } else if (i == 3) {
-        result = i - 3;
-        console.log(result);
-    } else if (i == 4) {
-        result = i * 4;
-        console.log(result);
-    } else if (i % 5 == 0) {
-        console.log(i)
+    sum = i;
+    if (i == 2 && i != 4) {
+        i += 2;
     }
-    result = i;
+    sum += i;
+    if (i == 3) {
+        i -= 3;
+    }
+    sum += i;
+    if (i == 4) {
+        i *= 4;
+    }
+    sum += i;
+    if (i % 5 == 0) {
+        console.log(i)
+        continue;
+    }
+    result = sum;
 }

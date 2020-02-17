232b0579e3568a81d200cf6d05c740f6cbeac025
Denis Olekh <denisolekh@gmail.com>
2020-02-17 16:35:37 +0200

    hw

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -4,11 +4,11 @@ let i = m;
 let result = 0;
 
 for (i = m; i <= n; i++) {
-    if (i % 2 == 0 && i % 4 == 1) {
+    if (i % 2 === 0 && i % 4 !== 1) {
         result += i;
-    } else if (i % 3 == 0) {
+    } else if (i % 3 === 0) {
         result -= i;
-    } else if (i % 4 == 0) {
+    } else if (i % 4 === 0) {
         result *= i;
 
     }

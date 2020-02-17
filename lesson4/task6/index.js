28db1563b4431487f23dcc7ca73920972ee96f70
Denis Olekh <denisolekh@gmail.com>
2020-02-17 16:55:02 +0200

    hw

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -3,8 +3,7 @@ for (i = 0; i <= 1000; i++) {
     if (i % 2 === 1) {
         console.log("Found");
         result += i;
-    }
-    if (result * 5 > 5000) {
+    } else if (result * 5 > 5000) {
         console.log("Bigger");
     } else console.log("Smaller or equal");
 }

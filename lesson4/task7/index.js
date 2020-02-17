51feec0a0ff0dbcf34a86169f21756db251303ec
Denis Olekh <denisolekh@gmail.com>
2020-02-17 17:42:54 +0200

    hw

--- lesson4/task7/index.js
+++ lesson4/task7/index.js
@@ -1,11 +1,11 @@
-let i = 0;
+let i = 1;
 let j = 0;
 let m = 1e7;
 let sum = 0;
 
-for (i = 0; i <= m; i++) {
-    for (j = 1; j <= i; j++) {
-        sum = j + i;
+for (i; i <= 1; i++) {
+    for (j = i; j <= m; j++) {
+        sum = (j + i) * (j - i + 1) / 2;
         console.log(sum);
     }
 }

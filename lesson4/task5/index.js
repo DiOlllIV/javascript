147519ca3fd86baa415122681f1c843a454d9e23
Denis Olekh <denisolekh@gmail.com>
2020-02-17 15:18:37 +0200

    hw

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -5,20 +5,24 @@ let sum = 0;
 let result;
 
 for (i = 0; i <= n; i++) {
-    sum += i;
+
     if (i % 2 == 0 && i % 4 == 1) {
         sum += 2;
         continue;
 
-    } else if (i / 3 == 0) {
+    }
+    sum += i;
+    if (i / 3 == 0) {
         sum -= 3;
         continue;
 
-    } else if (i / 4 == 0) {
+    }
+    sum += i;
+    if (i / 4 == 0) {
         sum *= 4;
         continue;
     }
-    result *= i;
+
 
     if (i % 5 == 0) {
         console.log(i);

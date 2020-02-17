85e2eb3e14306d36759a13cbea000f65c632d9c6
Denis Olekh <denisolekh@gmail.com>
2020-02-17 14:10:21 +0200

    hw

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -8,15 +8,11 @@ for (i = 0; i <= n; i++) {
     if (i % 2 == 0) {
         result += 2;
 
-    }
-    if (i / 3 == 0) {
+    } else if (i / 3 == 0) {
         result -= 3;
 
-    }
-    if (i / 4 == 0) {
-        result *= 4;
+    } else(i / 4 == 0)
 
-    }
     if (i % 5 == 0) {
         console.log(i);
     }

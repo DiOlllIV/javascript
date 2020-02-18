b8d37450a24dabf9679d97af79c9aba12425ef47
Denis Olekh <denisolekh@gmail.com>
2020-02-18 14:24:29 +0200

    hw

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,22 +1,26 @@
 let i = 1;
+let j = 1;
 
 function isPrimes(n) {
 
     for (i; i <= n; i++) {
+
         if (i % n === 0)
             return false;
         else return true;
     }
 
 }
+isPrimes(11);
 
 function getPrimes(n) {
-    while (1 >= n) {
-        if (isPrimes(n)) {
-            n--;
-            console.log(n);
+
+    for (j; j <= n; j++) {
+
+        if (isPrimes(j)) {
+
+            console.log(j);
         }
     }
 }
-
 getPrimes(11);

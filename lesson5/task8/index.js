ba4a002f03894f3a270385f5a7a7a6371a416d6f
Denis Olekh <denisolekh@gmail.com>
2020-02-18 17:35:55 +0200

    hw

--- lesson5/task8/index.js
+++ lesson5/task8/index.js
@@ -1,26 +1,26 @@
 function sum(from, to) {
     let res = 0;
 
-    /* if (from < to) { */
-    for (i = from; i <= to; i++) {
-        res += i;
-    }
-    //}  else
-    /*for (i = from; i >= to; i--) {
+    if (from < to) {
+        for (i = from; i <= to; i++) {
             res += i;
         }
- */
+    } else
+        for (i = from; i >= to; i--) {
+            res += i;
+        }
+
     return res;
 }
 
 function compareSums(a, b, c, d) {
-    /* let res1 = sum(a, b);
+    let res1 = sum(a, b);
     let res2 = sum(c, d);
- */
-    if (sum(a, b) > sum(c, d))
+
+    if (res1 > res2)
         return true;
     else
         return false;
 }
 
-compareSums(1, 2, 3, 4);
+compareSums(5, 2, 3, 1);

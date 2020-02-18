54a66234f902f08c21adca68e0f978d76701ac99
Denis Olekh <denisolekh@gmail.com>
2020-02-18 18:50:10 +0200

    hw

--- lesson5/task9/index.js
+++ lesson5/task9/index.js
@@ -1,6 +1,6 @@
 function findDivCount(a, b, n) {
     let quant = 0;
-    for (i = a; i <= b; i++) {
+    for (let i = a; i <= b; i++) {
         if (i % n === 0) {
             /* console.log(i); */
             quant++;

bd72643272dc159c1224a60fbeff4e7c8077b0d9
Denis Olekh <denisolekh@gmail.com>
2020-02-19 21:00:45 +0200

    hw

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,18 +1,20 @@
 function uniqueCount(array) {
     let unique = 0;
-    for (let i = 1; i < array.length; ++i) {
+    for (let i = 1; i <= array.length - 1; ++i) {
 
         let element = array[i];
 
         for (let j = i + 1; j < array.length; ++j) {
             if (array[j] === element) {
                 unique++;
+                console.log(element);
+                console.log(array[j]);
             }
         }
-        console.log(unique);
+
     }
     return unique;
 
 }
 
-console.log(uniqueCount([1, 2, 3, 1, 2, 3, 5, 4, 4, 6, 7, 1]));
+console.log(uniqueCount([1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 1, 6]));

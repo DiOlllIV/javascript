206f984e4e5173e0d6f27f7aaa8a4ecc44f83a1e
Denis Olekh <denisolekh@gmail.com>
2020-02-19 15:12:25 +0200

    hw

---
+++ lesson6/task5/index.js
@@ -0,0 +1,18 @@
+/* const arr = [1, 2, 3, 4]; */
+
+function squareArray(arr) {
+    let arrayInSquare = [];
+
+    if (!Array.isArray) {
+        return null;
+    }
+
+    for (let i = arr[0]; i <= arr.length; i++) {
+        arrayInSquare.push(i * 3);
+    }
+
+    return arrayInSquare;
+}
+
+/* const arrayInSquare = squareArray(arr);
+console.log(arrayInSquare); */

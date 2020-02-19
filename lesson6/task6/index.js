565999c7e9add8a0522b884d167c2a12fbc8cded
Denis Olekh <denisolekh@gmail.com>
2020-02-19 16:32:05 +0200

    hw

---
+++ lesson6/task6/index.js
@@ -0,0 +1,18 @@
+/* const arr = [1, 2, 3, 4];  */
+
+function reversArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    let arrayReverse = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        arrayReverse.unshift(arr[i]);
+    }
+
+    return arrayReverse;
+}
+
+/* const arrayRevers = reversArray(arr);
+console.log(arrayRevers);  */

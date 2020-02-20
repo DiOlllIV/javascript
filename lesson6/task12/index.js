81354331a0c50043c1086f6d9f66ff78d7454602
Denis Olekh <denisolekh@gmail.com>
2020-02-20 15:25:09 +0200

    hw

---
+++ lesson6/task12/index.js
@@ -0,0 +1,9 @@
+        function cloneArr(arr) {
+            arrClone = []
+            for (let i = 0; i < arr.length; ++i) {
+                arrClone.push(arr[i]);
+            }
+            return arrClone;
+        }
+
+        /*         console.log(cloeArr([3, 4, 5, 6, 7, 0, 1, 2])); */

3c49a352f63602f3e35957af3684a7320ef4bcd1
Denis Olekh <denisolekh@gmail.com>
2020-02-20 18:32:36 +0200

    hw

--- lesson6/task11/index.js
+++ lesson6/task11/index.js
@@ -1,6 +1,7 @@
 function checker(arr) {
     let minNum = arr[0];
     let maxNum = arr[0];
+
     for (i = 0; i < arr.length; i++) {
         console.log(arr[i]);
         if (minNum > arr[i])
@@ -8,8 +9,7 @@ function checker(arr) {
         if (maxNum < arr[i])
             maxNum = arr[i];
     }
-    console.log(minNum);
-    console.log(maxNum);
+
     if (minNum + maxNum > 1000)
         return true;
     else

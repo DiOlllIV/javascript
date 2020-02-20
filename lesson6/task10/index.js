fd9ed71cab2b1358a8413e4c41731ea8398ad574
Denis Olekh <denisolekh@gmail.com>
2020-02-20 17:19:05 +0200

    hw

--- lesson6/task10/index.js
+++ lesson6/task10/index.js
@@ -1,8 +1,8 @@
 function withdraw(clients, balances, client, amount) {
 
     for (let i = 0; i < clients.length; i++) {
-        balances[i] === client;
-        if (clients[i] === client) {
+
+        if (clients[i] === client && balances[i] === client) {
             if (amount > balances[i]) {
                 return -1;
             }

5a624eb486e082384ac4b38edd5d6b6eb5699ecc
Denis Olekh <denisolekh@gmail.com>
2020-02-21 19:43:48 +0200

    hw

--- lesson7/task1/index.js
+++ lesson7/task1/index.js
@@ -1,7 +1,5 @@
-function getSpecialNumbers(numbers) {
-
-    return numbers.filter(num => (num % 3 === 0));
-}
+const getSpecialNumbers = numbers =>
+    numbers.filter(num => num % 3 === 0);
 
 /* const arr = [1, 2, 3, 4, 7, 99];
 console.log(getSpecialNumbers(arr));

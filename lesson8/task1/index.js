9820e7e95e1a9d7005e9dc5a8a261513def14025
Denis Olekh <denisolekh@gmail.com>
2020-02-22 14:40:02 +0200

    hw

---
+++ lesson8/task1/index.js
@@ -0,0 +1,12 @@
+const transformToObject = arr => {
+    let obj = {};
+
+    arr.forEach(element => {
+        obj[element] = element;
+    });
+
+    return obj;
+};
+
+const arr = ['name', 'Tom', '22', 'value'];
+console.log(transformToObject(arr));

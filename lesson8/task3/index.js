07911df42a96331e5d99b3166edc691efa495904
Denis Olekh <denisolekh@gmail.com>
2020-02-22 15:27:30 +0200

    hw

---
+++ lesson8/task3/index.js
@@ -0,0 +1,19 @@
+const pickProps = (obj, arr) => {
+
+    let object = {};
+
+    for (let key in obj) {
+        for (let elm of arr) {
+            if (elm === key)
+                arr.forEach(elm => {
+                    object[elm] = obj[elm];
+                });
+        }
+    }
+
+    return object;
+};
+
+/* const arr = ['a', 'b'];
+const obj = { a: 1, b: 2, c: 3 };
+console.log(pickProps(obj, arr)); */

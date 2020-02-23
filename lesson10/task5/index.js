ed8adf0bb317a488485205b97c68aa0814afef2e
Denis Olekh <denisolekh@gmail.com>
2020-02-23 16:43:31 +0200

    hw

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,7 +1,7 @@
 const getTotalPrice = arr => {
     const sum = arr.reduce((acc, cur) => acc + cur);
 
-    return '$' + Number(sum.toFixed(2));
+    return '$' + Math.floor(num * 100) / 100;
 };
 
 /* const arr = [77.5434, 5.88, .5, 9];

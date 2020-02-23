8ce5cefb35fb628c7f62c9877a8270aac1076ec9
Denis Olekh <denisolekh@gmail.com>
2020-02-23 16:13:41 +0200

    hw

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,7 +1,7 @@
 const multiRound = num => [
-    Math.floor(num * 100) / 100,
-    Math.round(num * 100) / 100,
-    Math.ceil(num * 100) / 100,
-    Math.trunc(num * 100) / 100,
+    Number(Math.floor(num * 100) / 100),
+    Number(Math.round(num * 100) / 100),
+    Number(Math.ceil(num * 100) / 100),
+    Number(Math.trunc(num * 100) / 100),
     Number(num.toFixex(2))
 ];

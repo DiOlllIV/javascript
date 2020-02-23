b17e4324f0eb297e225fbb3080dbe182e3a1d5e6
Denis Olekh <denisolekh@gmail.com>
2020-02-23 20:45:20 +0200

    hw

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -2,12 +2,11 @@ const getRandomNumbers = (length, start, end) => {
     if (Math.floor(start) >= Math.floor(end))
         return null;
 
-    const arr = [];
-    arr.length = length;
-
-    return arr.fill().map(num =>
-        Math.floor(Math.random() * (end - start + 1) + start));
+    /*     const arr = [];
+        arr.length = length;
+     */
+    return Array(length).fill().map(num =>
+        Math.floor(Math.random() * (start - end + 1) + end));
 };
 
-
-console.log(getRandomNumbers(5, 1.4, 7));
+/* console.log(getRandomNumbers(5, 1.4, 7)); */

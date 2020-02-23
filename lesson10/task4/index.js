1ce27db4f737a06787d76322f0b66061607b3923
Denis Olekh <denisolekh@gmail.com>
2020-02-23 16:27:51 +0200

    hw

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,5 +1,13 @@
 const getMaxAbsoluteNumber = arr => {
+
+    if (!Array.isArray(arr) || arr == 0)
+        return null;
+
     const absolute = arr.map(elem => Math.abs(elem));
 
     return Math.max(...absolute);
 };
+/* 
+const arr = [-99, 4, 777, 88, -999];
+const arr1 = [];
+console.log(getMaxAbsoluteNumber(arr1)) */

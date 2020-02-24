8f2936877e3b958111b3d75eb76b972cc81a1024
Denis Olekh <denisolekh@gmail.com>
2020-02-24 16:54:25 +0200

    hw

---
+++ lesson11/task4/index.js
@@ -0,0 +1,21 @@
+const countOccurences = (findStr, str) => {
+
+    let sum = 0;
+    let pos = 0;
+
+    while (true) {
+        let foundPos = str.indexOf(findStr, pos);
+
+        if (foundPos == -1)
+            break;
+
+        sum++;
+        pos = foundPos + 1;
+    }
+
+    return sum;
+};
+
+
+/* let text = "Lorem Ipsum s simpy dunny text of the printing and typesetting index has been the industry's standard dummy text ever since the 1500s.";
+console.log(countOccurences('s', text)); */

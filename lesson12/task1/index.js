ebe74b44b9e71d913c6a8e1b2e61132b6e9e1af2
Denis Olekh <denisolekh@gmail.com>
2020-02-24 21:31:33 +0200

    hw

---
+++ lesson12/task1/index.js
@@ -0,0 +1,28 @@
+const calc = expression => {
+
+    const [a, oper, b] = expression.split(' ');
+    let result;
+
+    switch (oper) {
+        case '+':
+            result = Number(a) + Number(b);
+            break;
+        case '-':
+            result = Number(a) - Number(b);
+            break;
+        case '*':
+            result = Number(a) * Number(b);
+            break;
+        case '/':
+            result = Number(a) / Number(b);
+            break;
+
+    }
+
+    return `${expression} = ${result}`;
+}
+
+console.log(calc('1 + 2'));
+console.log(calc('1 - 2'));
+console.log(calc('1 * 2'));
+console.log(calc('1 / 2'));

109380cb58592a2173563093034aba6796a1c880
Denis Olekh <denisolekh@gmail.com>
2020-02-24 15:57:59 +0200

    hw

---
+++ lesson11/task3/index.js
@@ -0,0 +1,29 @@
+const splitString = (string, len) => {
+
+    if (typeof string != 'string')
+        return null;
+
+    const strArr = [];
+    let start = 0;
+
+    while (true) {
+        let chunk = text.substring(start, len);
+
+        if (chunk.length === 0)
+            break;
+
+        // if length of chunk smaller than length of substring, 
+        // repeats dot to remaining length
+        if (chunk.length < len)
+            chunk += '.'.repeat(len - chunk.length)
+
+        strArr.push(chunk.slice());
+        start += len;
+    }
+
+    return strArr;
+};
+
+/* 
+let text = "Lorem Ipsum s simpy dunny text of the printing and typesetting index has been the industry's standard dummy text ever since the 1500s.";
+console.log(splitString(text, 3)); */

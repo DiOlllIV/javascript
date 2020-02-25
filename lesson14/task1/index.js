aabfe253de8c22968e68fd49b64f20d9ec601b3c
Denis Olekh <denisolekh@gmail.com>
2020-02-25 22:57:15 +0200

    hw

---
+++ lesson14/task1/index.js
@@ -0,0 +1,11 @@
+let message = 'Just learn it!';
+
+function sendMessage(name) {
+    const sender = 'Your Gromecode';
+
+    console.log(`${name}, ${message} ${sender}`);
+};
+
+function setMessage(text) {
+    message = text;
+};

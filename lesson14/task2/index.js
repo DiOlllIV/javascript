b54c5fec644bf8265b55316855a1913332def977
Denis Olekh <denisolekh@gmail.com>
2020-02-26 10:39:28 +0200

    hw

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -3,7 +3,7 @@ export default function createMessenger() {
     let message = 'Just learn it!';
     let sender = 'Gromcode';
 
-    function sendMessage(name) {
+    const sendMessage = name => {
 
         console.log(`${name}, ${message} Your ${sender}`);
     };

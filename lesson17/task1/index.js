4e1a68a5e6be8efbd1c8e098f0ca810d0d991cb7
Denis Olekh <denisolekh@gmail.com>
2020-02-27 15:49:50 +0200

    hw

---
+++ lesson17/task1/index.js
@@ -0,0 +1,11 @@
+let user = {
+    lastName: 'Doe',
+    firstName: 'John',
+    getFullName() {
+        console.log(`${this.firstName} ${this.lastName}`)
+    },
+};
+
+user.getFullName();
+
+export { user };

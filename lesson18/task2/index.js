33abc6d018d388a219149365c1f747e010291a3b
Denis Olekh <denisolekh@gmail.com>
2020-02-28 14:36:06 +0200

    hw

--- lesson18/task2/index.js
+++ lesson18/task2/index.js
@@ -1,12 +1,12 @@
 const wallet = {
     transactions: [33, 565, 678, 23, 55, 77, 007],
 
-    getMin() {
-        return Math.min(...this.transactions);
-    },
-
     getMax() {
         return Math.max(...this.transactions);
+    },
+
+    getMin() {
+        return Math.min(...this.transactions);
     },
 };
 

350acb37fb292bdc460ba3b5b7624d4250713a43
Denis Olekh <denisolekh@gmail.com>
2020-02-28 23:16:13 +0200

    hw

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -3,7 +3,7 @@ export function getOwnProps(obj) {
     let propsArr = [];
 
     for (let prop in obj) {
-        if (typeof obj[prop] !== 'function')
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function')
             propsArr.push(prop);
     }
 

0ea163669a94e34bc9b72233c48dd7144ddd907b
Denis Olekh <denisolekh@gmail.com>
2020-02-29 18:39:31 +0200

    hw

---
+++ lesson20/task4/index.js
@@ -0,0 +1,30 @@
+class Order {
+
+    constructor(price, city, type) {
+        this.id = Math.ceil(Math.random(0, this.id + 1)).toString();
+        this.price = price;
+        this.city = city;
+        this.type = type;
+        this.dateCreated = new Date;
+        this.dateConfirmed;
+        this.isConfirmed = false;
+    }
+
+    checkPricec() {
+        if (this.price > 1000)
+            return true;
+        else return false;
+    };
+
+    confirmOrder() {
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date;
+    };
+
+    isValidType() {
+        if (this.type === 'Buy' || this.type === 'Sell')
+            return true;
+        else return false;
+    }
+
+};

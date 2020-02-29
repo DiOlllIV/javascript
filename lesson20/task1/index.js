392966aa0b7fdddc3caad59b8f4a52d5c3403913
Denis Olekh <denisolekh@gmail.com>
2020-02-29 15:03:29 +0200

    hw

---
+++ lesson20/task1/index.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi() {
+        console.log(`$Hi, I am ${this.name}`);
+    }
+
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+
+    setAge(value) {
+        if (value < 0)
+            return false;
+
+        this.age = value;
+
+        if (value >= 25)
+            this.requestNewPhoto();
+
+        return values;
+    }
+
+    static createEmpty() {
+        return new this('', null);
+    }
+};

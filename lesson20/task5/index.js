1321bc31d5f8d813f071fc88c258af4bfef1f832
Denis Olekh <denisolekh@gmail.com>
2020-02-29 19:11:34 +0200

    hw

---
+++ lesson20/task5/index.js
@@ -0,0 +1,30 @@
+class User {
+    #id;
+    #name;
+    #sessionId;
+
+    constructor(id, name, sessionId) {
+        this.#id = id.toString();
+        this.#name = name.toString();
+        this.#sessionId = sessionId.toString();
+    }
+};
+
+
+class UserRepository extends User {
+    users = [].push({ User });
+
+    getUserName() {
+        this.users.filter(key => key.this.name);
+    };
+
+    getUserIds() {
+        this.users.filter(key => key.this.id);
+    };
+
+    getUserNameById(id) {
+        if (id === this.id)
+            return this.name;
+    };
+
+}

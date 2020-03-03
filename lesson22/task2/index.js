8ce54333f72afdc830bc091390cd6b29bbe089aa
Denis Olekh <denisolekh@gmail.com>
2020-03-04 00:43:32 +0200

    hw

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,11 +1,4 @@
-const tasks = [
-    { text: 'Buy milk', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: false },
-    { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
-];
-
+const tasks = [];
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');

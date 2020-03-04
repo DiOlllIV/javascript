9faa0a1e1144c2f2840f4508e0f5e837f2a9f7aa
Denis Olekh <denisolekh@gmail.com>
2020-03-04 16:04:58 +0200

    hw

---
+++ lesson24/task2/index.js
@@ -0,0 +1,10 @@
+const formatter = new Intl.DateTimeFormat('en', {
+    timeZone: 'GMT',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+});
+
+const getGreenwichTime = date => formatter.format(date);
+
+console.log(getGreenwichTime(new Date));

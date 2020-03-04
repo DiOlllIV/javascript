3c7eaf4e8ec64360d05873d9100d7040fc92917a
Denis Olekh <denisolekh@gmail.com>
2020-03-04 19:47:07 +0200

    hw

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -4,8 +4,8 @@ const getDiff = (startDate, endDate) => {
         diff = new Date(endDate) - new Date(startDate);
 
     else diff = new Date(startDate) - new Date(endDate);
-    const daysDiff = Math.floor(diff / (1000 * 60 * 60 * 24));
 
+    const daysDiff = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hourDiff = Math.floor((diff / 1000 / 60 / 60) % 24);
     const minDiff = Math.floor((diff / 1000 / 60) % 60);
     const secDiff = Math.floor((diff / 1000) % 60);

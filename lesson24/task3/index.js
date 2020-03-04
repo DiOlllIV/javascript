875df95013c78147ea00ae9c205c5eee092990a2
Denis Olekh <denisolekh@gmail.com>
2020-03-04 21:32:36 +0200

    hw

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,16 +1,13 @@
 const getDiff = (startDate, endDate) => {
 
-    if (startDate < endDate)
-        diff = new Date(endDate) - new Date(startDate);
-
-    else diff = new Date(startDate) - new Date(endDate);
+    diff = new Date(startDate) - new Date(endDate);
 
     const daysDiff = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hourDiff = Math.floor((diff / 1000 / 60 / 60) % 24);
     const minDiff = Math.floor((diff / 1000 / 60) % 60);
     const secDiff = Math.floor((diff / 1000) % 60);
 
-    return `${daysDiff}d ${hourDiff}h ${minDiff}m ${secDiff}s`;
+    return `${Math.abs(daysDiff)}d ${Math.abs(hourDiff)}h ${Math.abs(minDiff)}m ${Math.abs(secDiff)}s`;
 };
 
 /* console.log(getDiff('1992-03-04 22:22:22', '1993-03-04 00:00:00')); */

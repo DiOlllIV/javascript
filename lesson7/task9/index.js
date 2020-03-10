1b6ea996cc9eca1f991588721be2bcc156b193a1
Denis Olekh <denisolekh@gmail.com>
2020-03-10 18:11:27 +0200

    hw

---
+++ lesson7/task9/index.js
@@ -0,0 +1,9 @@
+function withdraw(clients, balances, client, amount) {
+
+    let clientId = clients.findIndex(elem => (elem === client));
+
+    return balances[clientId] = amount > balances[clientId] ?
+        -1 : balances[clientId] -= amount;
+
+
+};

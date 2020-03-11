f71dddcad80515a0bc20b7711854be63f39aa203
Denis Olekh <denisolekh@gmail.com>
2020-03-11 14:37:26 +0200

    hw

---
+++ lesson21/task3/index.js
@@ -0,0 +1,13 @@
+function getItemsList() {
+    const technologyItems = document.querySelectorAll('.technology');
+
+    console.dir(technologyItems);
+    return technologyItems;
+};
+
+function getItemsArray() {
+    const elementsArray = Array.from(document.querySelectorAll('.tool'));
+
+    console.dir(elementsArray);
+    return elementsArray;
+};

7a4a17bd7494b7fd011f132592e9584ac3fb4ea2
Denis Olekh <denisolekh@gmail.com>
2020-03-11 18:52:50 +0200

    hw

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -3,17 +3,17 @@ function finishList() {
     const special = document.querySelector('.special');
 
     const li1 = document.createElement('li');
-    li1.innerText = '1';
+    li1.textContent = '1';
 
     const li4 = document.createElement('li');
-    li4.innerText = '4';
+    li4.textContent = '4';
 
 
     const li6 = document.createElement('li');
-    li6.innerText = '6';
+    li6.textContent = '6';
 
     const li8 = document.createElement('li');
-    li8.innerText = '8';
+    li8.textContent = '8';
 
     list.prepend(li1);
     special.before(li4)

9a19b27fd5fce6e2ec97ff0c6a54517105905a99
Denis Olekh <denisolekh@gmail.com>
2020-03-11 15:39:51 +0200

    hw

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,8 +1,8 @@
 function setButton(buttonText) {
     const body = document.querySelector('body');
 
-    body.textContent = buttonText;
-    body.innerHTML += buttonText;
+    /* body.textContent = buttonText; */
+    body.innerHTML = buttonText;
 };
 
 /* setButton("<button>button text</button>"); */

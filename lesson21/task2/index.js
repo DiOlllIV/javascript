c73a544789dd6029b2e512f9a482318dbc12f4b2
Denis Olekh <denisolekh@gmail.com>
2020-03-11 13:59:33 +0200

    hw

---
+++ lesson21/task2/index.js
@@ -0,0 +1,13 @@
+export default function() {
+    const title = document.querySelector('.title');
+
+    console.log(title);
+    return title;
+};
+
+export default function() {
+    const input = document.querySelector('input');
+
+    console.dir(input);
+    return input;
+};

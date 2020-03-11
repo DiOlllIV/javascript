9d7cfc5715c1c797f1819f13dd1e649dc29e954b
Denis Olekh <denisolekh@gmail.com>
2020-03-11 21:24:42 +0200

    hw

---
+++ lesson21/task12/index.js
@@ -0,0 +1,10 @@
+function squaredNumber() {
+    const liArr = document.querySelectorAll('.number');
+
+    for (let elem of liArr) {
+        const numInSquare = elem.getAttribute('data-number') * elem.getAttribute('data-number');
+        elem.dataset.squaredNumber = numInSquare;
+    }
+};
+
+export { squaredNumber };

9a3f8ca07dd8f44436118c3e9723b03b47c3be51
Denis Olekh <denisolekh@gmail.com>
2020-03-12 14:19:04 +0200

    hw

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,3 +1,3 @@
 const button = document.querySelector('.create-btn');
 
-button.addEventListener('onclick', () => { alert('done'), false });
+button.addEventListener('click', () => { alert('done'), false });

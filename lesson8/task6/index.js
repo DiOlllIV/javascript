f7ffef4ad6b9d412565e1d8f1c52bd0c6eb04de8
Denis Olekh <denisolekh@gmail.com>
2020-03-14 18:40:23 +0200

    hw

--- lesson8/task6/index.js
+++ lesson8/task6/index.js
@@ -1,10 +1,11 @@
-const user = {
+/* const user = {
     name: "Sam",
-}
+} */
 
-const addPropertyV1 = (userData, userId) =>
+const addPropertyV1 = (userData, userId) => {
     userData.id = userId;
-
+    return userData
+};
 const addPropertyV2 = (userData, userId) =>
     Object.assign(userData, userData.id = userId);
 
@@ -20,3 +21,8 @@ const addPropertyV4 = (userData, userId) => {
     objCopy.id = userId;
     return objCopy;
 };
+
+/* const v1 = addPropertyV1(user, 1);
+const v2 = addPropertyV2(user, 2);
+const v3 = addPropertyV3(user, 3);
+const v4 = addPropertyV4(user, 4); */

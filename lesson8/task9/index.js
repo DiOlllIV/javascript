58a716519bed08a8e861dcef48cfaebd3e002a8b
Denis Olekh <denisolekh@gmail.com>
2020-03-14 19:40:56 +0200

    hw

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -20,8 +20,11 @@ const obj4 = {
 }; */
 
 const compareObjects = (obj1, obj2) => {
+
     for (let key1 in obj1) {
+
         for (let key2 in obj2) {
+
             return (key1 === key2 && obj1[key1] === obj2[key2]) ?
                 true : false;
         }

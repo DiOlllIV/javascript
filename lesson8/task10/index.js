d952ab6524f55511803b3eb79294b0aaf37d6d2e
Denis Olekh <denisolekh@gmail.com>
2020-03-15 15:02:30 +0200

    hw

---
+++ lesson8/task10/index.js
@@ -0,0 +1,19 @@
+const keys = ['name', 'addres', 'age'];
+const values = ['Bob', 'Ukraine', 34];
+
+const buildObject = (keysList, valuesList) => {
+
+    const arr = [];
+
+    for (let i = 0; i < keysList.length; i++) {
+        arr.push([keysList[i], valuesList[i]]);
+
+    }
+    const map = new Map(arr);
+    const obj = Object.fromEntries(map);
+    console.log(obj);
+
+};
+
+
+buildObject(keys, values);

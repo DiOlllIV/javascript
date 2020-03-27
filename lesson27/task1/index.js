299eef633f431366fb1780dd4c6d655a9cd02ee5
Denis Olekh <denisolekh@gmail.com>
2020-03-27 10:28:32 +0200

    hw

---
+++ lesson27/task1/index.js
@@ -0,0 +1,18 @@
+const getLocalStorageData = () => {
+    Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+
+            try {
+                newValue = JSON.parse(value);
+            } catch {
+                newValue = value;
+            }
+
+            return {
+                ...acc,
+                [key]: newValue,
+            }
+
+        }, {});
+};

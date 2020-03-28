d4d25bc273586e5cfc79b826fe1aca619cdd29ee
Denis Olekh <denisolekh@gmail.com>
2020-03-28 17:40:11 +0200

    hw

--- lesson28/task7/index.js
+++ lesson28/task7/index.js
@@ -1,3 +1,11 @@
 export const sum = num =>
     num === 1 ? 1 :
     num + sum(num - 1);
+
+/* const sum = num => {
+    let arr = [];
+    for (let i = 0; i <= num; i++) {
+        arr.push(i);
+    }
+    return arr.reduce((acc, rec) => acc + rec);
+}; */

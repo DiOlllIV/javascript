80e8d9ee96ff50a7442fc85774026fff4bf9e06f
Denis Olekh <denisolekh@gmail.com>
2020-03-29 21:12:24 +0300

    hw

--- lesson30/task2/index.js
+++ lesson30/task2/index.js
@@ -1,3 +1,5 @@
+import { addImage } from './addImage.js'
+
 export const addImageV2 = imgSrc => {
     return new Promise((resolve, reject) => {
         addImage(imgSrc, (error, data) => {
@@ -9,24 +11,4 @@ export const addImageV2 = imgSrc => {
             resolve(data);
         });
     });
-};
-
-const addImage = (url, callback) => {
-    const img = document.createElement('img');
-    img.setAttribute('alt', 'User avatar');
-    img.src = url;
-
-    const pageElem = document.querySelector('.page');
-    pageElem.append(img);
-
-    const onImageLoaded = () => {
-        const { width, height } = img;
-        callback(null, { width, height });
-    }
-
-    const onImageLoadError = () => callback('Image load failed');
-
-    img.addEventListener('load', onImageLoaded);
-
-    img.addEventListener('error', onImageLoadError);
 };

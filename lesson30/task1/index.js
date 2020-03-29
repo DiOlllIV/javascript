754b3e9b2d0ef8ac913b3fbe74a930c276037431
Denis Olekh <denisolekh@gmail.com>
2020-03-29 15:38:58 +0300

    hw

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -20,6 +20,9 @@ export const addImage = imgSrc => {
     return p;
 };
 
+const imgSrc = 'http://img10.joyreactor.c/pics/post/Anastasia-Savina-%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0-%D0%BC%D0%B0%D0%BB%D0%B5%D0%BD%D1%8C%D0%BA%D0%B0%D1%8F-%D0%B3%D1%80%D1%83%D0%B4%D1%8C-%D1%81%D0%B8%D1%81%D1%8C%D0%BA%D0%B8-5810779.jpeg';
+const promise = addImageWithPromise(imgSrc);
+
 const ifReject = error => {
     console.log(error);
     return;

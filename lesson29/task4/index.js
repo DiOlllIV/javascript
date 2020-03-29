a56bd3ec4023bcec7798fc8a3753b773821c8628
Denis Olekh <denisolekh@gmail.com>
2020-03-29 13:38:27 +0300

    hw

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,18 +1,17 @@
 export const requestUserData = (userId, callback) => {
-    const interval = (Math.random() * (3 - 1)) + 1;
+    const interval = (Math.random() * (3 - 1) + 1) * 1000;
     setTimeout(() => {
 
         if (userId === 'broken') {
             callback(null, 'Failed to load user data');
             return;
         }
-        if (userId === 'userId777') {
-            callback({
-                name: 'John',
-                age: 17,
-                userId: 'userId777',
-                email: 'userId777@example.com'
-            })
-        }
+
+        callback({
+            name: 'John',
+            age: 17,
+            userId,
+            email: `${userId}@example.com`,
+        })
     }, interval);
 };

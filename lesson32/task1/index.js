025683850745421ee0e11dfaf59c575ee7cb69a7
Denis Olekh <denisolekh@gmail.com>
2020-03-30 14:09:37 +0300

    hw

---
+++ lesson32/task1/index.js
@@ -0,0 +1,24 @@
+const getValueWithDelay = (value, delay) =>
+    new Promise(resolve =>
+        setTimeout(() => {
+            console.log(value);
+            resolve(value);
+        }, delay));
+
+const asyncNum1 = getValueWithDelay(56, 1000);
+const asyncNum2 = getValueWithDelay(undefined, 2000);
+const asyncNum3 = getValueWithDelay('6', 3000);
+
+const getSum = numbers =>
+    numbers
+    .filter(value => !isNaN(value))
+    .reduce((acc, rec) => acc + Number(rec), 0);
+
+export const asyncSum = (...asyncNumbers) =>
+    Promise.all(asyncNumbers)
+    .then(num => getSum(num))
+    .catch(() =>
+        Promise.reject(new Error("Can't calculate")));
+
+asyncSum(asyncNum1, asyncNum2, asyncNum3)
+    .then(result => console.log(result));

a6d0a1e29efa59507a78c0bdc0e54f43ed02e5ac
Denis Olekh <denisolekh@gmail.com>
2020-03-30 11:12:26 +0300

    hw

---
+++ lesson31/task1/index.js
@@ -0,0 +1,17 @@
+export const requestUserData = userId =>
+    new Promise((rulfilled, reject) => {
+        if (userId === ' broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                rulfilled({
+                    name: 'John',
+                    age: 17,
+                    userId,
+                    email: `${userId}@example.com`,
+                });
+            }, 1000);
+        }
+    });

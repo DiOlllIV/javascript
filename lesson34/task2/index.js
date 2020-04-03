f0a3fbb20ccb8b3b2ac63f00f00f06e5824c47b4
Denis Olekh <denisolekh@gmail.com>
2020-04-03 11:01:03 +0300

    hw

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://crudcrud.com/api/8e1527448f6c49ce90a6ec5e58d8d97f/tasks'
 
 const loginForm = document.querySelector('.login-form');
-const inputs = [...document.querySelectorAll('input')];
+const inputs = document.querySelectorAll('input');
 const submitBtn = document.querySelector('.submit-button');
 const errorText = document.querySelector('.error-text');
 
@@ -18,8 +18,8 @@ const validationUser = e => {
     e.preventDefault();
 
     const userValue = [...new FormData(loginForm)]
-        .reduce((acc, [key, value]) =>
-            ({...acc, [key]: value }), {});
+        .reduce((email, [name, pass]) =>
+            ({...email, [name]: pass }), {});
 
     fetch(baseUrl, {
             method: 'POST',
@@ -31,7 +31,6 @@ const validationUser = e => {
         .then(response => response.json())
         .then(data => {
             inputs.map(elem => elem.value = '');
-            submitBtn.disabled = true;
             alert(JSON.stringify(data));
         })
         .catch(() => {

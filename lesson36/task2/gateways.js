4301df71cb2f201ebbdca8897d0301fb54cbc8ed
Denis Olekh <denisolekh@gmail.com>
2020-04-06 15:12:14 +0300

    hw

---
+++ lesson36/task2/gateways.js
@@ -0,0 +1,17 @@
+//https://api.github.com/users/USERNAME
+//fetch and render user data
+export const fetchUserData = async userName => {
+    const response = await fetch(`https://api.github.com/users/${userName}`)
+    if (response.ok)
+        return await response.json();
+
+    throw new Error('Failed to load');
+};
+
+export const fetchRepositories = async url => {
+    const response = await fetch(url)
+    if (response.ok)
+        return await response.json();
+
+    throw new Error('Failed to load');
+};
---
+++ lesson36/task2/index.js
@@ -0,0 +1,48 @@
+import { fetchUserData, fetchRepositories } from './gateways.js'
+import { renderUserData } from './user.js'
+import { renderRepos, cleanReposList } from './repos.js'
+import { showSpinner, hiddenSpinner } from './spinner.js'
+
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+const defaultUser = {
+    avatar_url: defaultAvatar,
+    name: '',
+    location: '',
+};
+
+renderUserData(defaultUser);
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const onSearchUser = async() => {
+    showSpinner();
+    cleanReposList();
+    const userName = userNameInputElem.value;
+    try {
+        const userData = await fetchUserData(userName);
+        renderUserData(userData);
+        const reposList = await fetchRepositories(userData.repos_url);
+        renderRepos(reposList);
+    } catch (err) {
+        alert(err.message);
+    } finally {
+        hiddenSpinner();
+    }
+
+    /* fetchUserData(userName)
+        .then(userData => {
+            renderUserData(userData);
+            return userData.repos_url;
+        })
+        .then(url => fetchRepositories(url))
+        .then(reposList => {
+            renderRepos(reposList);
+        })
+        .catch(err => {
+            alert(err.message);
+        })
+        .finally(() => hiddenSpinner()); */
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);

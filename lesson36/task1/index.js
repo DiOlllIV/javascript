5bd9df38a9eda77bbd98c34fcaf5bb1374a3168f
Denis Olekh <denisolekh@gmail.com>
2020-04-06 15:08:41 +0300

    hw

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,11 +1,12 @@
 export const fetchUser = async userId => {
-    const response = await fetch(`https://api.github.com/users/${userId}`);
-
     try {
+        const response = await fetch(`https://api.github.com/users/${userId}`);
+
         if (!response.ok)
             return null;
 
         const userData = await response.json();
+
         return userData;
     } catch {
         throw new Error('Failed to fetch user');

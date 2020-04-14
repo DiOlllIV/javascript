d970c591ab09beedddf42b53966db8b7ab6edd1d
Denis Olekh <denisolekh@gmail.com>
2020-04-14 12:54:59 +0300

    hw

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,43 +1,20 @@
-    const checkEmail = (arr, data) =>
-        arr.find(obj =>
-            obj.email === data);
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-    const countCommits = arr =>
-        arr.reduce((acc, cur) => {
-            const confirm = checkEmail(acc, cur.commit.author.email);
-            if (confirm === undefined) {
-                const obj = {
-                    count: 1,
-                    name: cur.commit.author.name,
-                    email: cur.commit.author.email
-                };
-                acc.push(obj);
-            } else confirm.count++;
+export const getTasksList = () =>
+fetch(baseUrl)
+.then(response =>
+    response.json());
 
-            return acc;
-        }, [{}]);
+export const getTaskById = (id) =>
+fetch(`https://api.github.com/users/${id}`)
+.then(response =>
+    response.json());
 
-    const filterActiviti = arr => {
-        arr.sort((a, b) => b.count - a.count);
-        const first = arr[0].count;
+/* примеры использования */
+getTasksList().then(tasksList => {
+    console.log(tasksList); // [{"id":"1", "done":false ... }, {"id":"2", "done":true ... }, ...]
+});
 
-        return arr.filter(obj => obj.count === first);
-    };
-
-    const fetchActivities = (from, userId, repoId) =>
-        fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
-        .then(commits => commits.json())
-        .then(data => data.filter(obj =>
-            Date.parse(`${obj.commit.author.date}`) > from))
-        .then(obj => countCommits(obj).splice(1))
-        .then(result => filterActiviti(result));
-
-    const getStartDate = days => new Date().setDate(new Date().getDate() - days);
-
-    export const getMostActiveDevs = data => {
-        const { days, userId, repoId } = data;
-        const from = getStartDate(days);
-
-        console.log(fetchActivities(from, userId, repoId));
-        return fetchActivities(from, userId, repoId);
-    };
+getTaskById('2').then(taskData => {
+    console.log(taskData); // {"id":"1", "done":false ... }
+});
---
+++ lesson33/task3/index.js
@@ -0,0 +1,43 @@
+    const checkEmail = (arr, data) =>
+        arr.find(obj =>
+            obj.email === data);
+
+    const countCommits = arr =>
+        arr.reduce((acc, cur) => {
+            const confirm = checkEmail(acc, cur.commit.author.email);
+            if (confirm === undefined) {
+                const obj = {
+                    count: 1,
+                    name: cur.commit.author.name,
+                    email: cur.commit.author.email
+                };
+                acc.push(obj);
+            } else confirm.count++;
+
+            return acc;
+        }, [{}]);
+
+    const filterActiviti = arr => {
+        arr.sort((a, b) => b.count - a.count);
+        const first = arr[0].count;
+
+        return arr.filter(obj => obj.count === first);
+    };
+
+    const fetchActivities = (from, userId, repoId) =>
+        fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
+        .then(commits => commits.json())
+        .then(data => data.filter(obj =>
+            Date.parse(`${obj.commit.author.date}`) > from))
+        .then(obj => countCommits(obj).splice(1))
+        .then(result => filterActiviti(result));
+
+    const getStartDate = days => new Date().setDate(new Date().getDate() - days);
+
+    export const getMostActiveDevs = data => {
+        const { days, userId, repoId } = data;
+        const from = getStartDate(days);
+
+        console.log(fetchActivities(from, userId, repoId));
+        return fetchActivities(from, userId, repoId);
+    };
